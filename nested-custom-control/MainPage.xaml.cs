19cc39246f20054da7fae4b0dde314286f939020
Tom Gregor <tgregor@ivsoftware.com>
2023-12-02 08:05:10 -0700

    rp

--- nested-custom-control/MainPage.xaml.cs
+++ nested-custom-control/MainPage.xaml.cs
@@ -1,25 +1,60 @@
-﻿namespace nested_custom_control
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace nested_custom_control
 {
     public partial class MainPage : ContentPage
     {
-        int count = 0;
-
         public MainPage()
         {
             InitializeComponent();
         }
-
-        private void OnCounterClicked(object sender, EventArgs e)
-        {
-            count++;
-
-            if (count == 1)
-                CounterBtn.Text = $"Clicked {count} time";
-            else
-                CounterBtn.Text = $"Clicked {count} times";
-
-            SemanticScreenReader.Announce(CounterBtn.Text);
-        }
     }
+    class MainPageBindingContext : INotifyPropertyChanged
+    {
+        public string Text1
+        {
+            get => _text1;
+            set
+            {
+                if (!Equals(_text1, value))
+                {
+                    _text1 = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+        string _text1 = string.Empty;
+        public string Text2
+        {
+            get => _text2;
+            set
+            {
+                if (!Equals(_text2, value))
+                {
+                    _text2 = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+        string _text2 = string.Empty;
+        public string Text3
+        {
+            get => _text3;
+            set
+            {
+                if (!Equals(_text3, value))
+                {
+                    _text3 = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+        string _text3 = string.Empty;
 
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null) =>
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+    }
 }
